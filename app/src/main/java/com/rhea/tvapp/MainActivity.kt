108f6984d1853199d33fdb5a92257297aad90227
Rhea <rheachhabra1011@gmail.com>
2023-12-04 17:27:01 +0530

    side menu init

--- app/src/main/java/com/rhea/tvapp/MainActivity.kt
+++ app/src/main/java/com/rhea/tvapp/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.rhea.tvapp
 
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.activity.viewModels
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.lifecycleScope
 import com.rhea.tvapp.databinding.ActivityMainBinding
+import com.rhea.tvapp.util.Util
 import com.rhea.tvapp.view.HomeFragment
 import com.rhea.tvapp.view.MainViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
-class MainActivity: FragmentActivity() {
+class MainActivity: FragmentActivity(), View.OnKeyListener {
 
     private val viewModel: MainViewModel by viewModels()
 
@@ -15,16 +23,69 @@ class MainActivity: FragmentActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private var lastSelectedMenu: View? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         addHomeFragment()
+        setLiveDataObserver()
+        setKeyListeners()
+    }
+
+    private fun setKeyListeners() {
+        binding.apply {
+            btnGenre.setOnKeyListener(this@MainActivity)
+            btnHome.setOnKeyListener(this@MainActivity)
+            btnLanguage.setOnKeyListener(this@MainActivity)
+            btnSearch.setOnKeyListener(this@MainActivity)
+            btnSettings.setOnKeyListener(this@MainActivity)
+            btnSports.setOnKeyListener(this@MainActivity)
+            btnTv.setOnKeyListener(this@MainActivity)
+            btnMovies.setOnKeyListener(this@MainActivity)
+        }
+    }
+
+    private fun setLiveDataObserver() {
+        lifecycleScope.launch {
+            viewModel.isSideMenuOpened.collectLatest {
+                toggleSideMenu(it)
+            }
+        }
+    }
+
+    private fun toggleSideMenu(open: Boolean) {
+        binding.apply {
+            val width = if (open) 16 else 5
+            blfNavBar.requestLayout()
+            blfNavBar.layoutParams.width = Util.getWidthInPercent(this@MainActivity, width)
+            if (!open)
+                container.requestFocus()
+            else lastSelectedMenu?.requestFocus()
+        }
     }
 
     private fun addHomeFragment() {
         supportFragmentManager.beginTransaction().apply {
-            replace(R.id.home_fragment, HomeFragment())
+            replace(R.id.container, HomeFragment())
             commit()
         }
+    }
+    override fun onKey(view: View?, eventId: Int, event: KeyEvent?): Boolean {
+        when(eventId) {
+            KeyEvent.KEYCODE_DPAD_LEFT -> viewModel.isSideMenuOpened.value = true
+            KeyEvent.KEYCODE_DPAD_CENTER -> {
+                if (view == null ) return false
+                lastSelectedMenu = view
+            }
+        }
+        return false
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_DPAD_RIGHT) {
+           viewModel.isSideMenuOpened.value = false
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
--- app/src/main/java/com/rhea/tvapp/view/MainViewModel.kt
+++ app/src/main/java/com/rhea/tvapp/view/MainViewModel.kt
@@ -6,10 +6,12 @@ import androidx.lifecycle.ViewModel
 import com.rhea.tvapp.data.Movie
 import com.rhea.tvapp.data.Movies
 import com.rhea.tvapp.util.Util
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class MainViewModel: ViewModel() {
 
     val selectedMovie = MutableLiveData<Movie>()
+    val isSideMenuOpened = MutableStateFlow(false)
 
     fun getListData(assets: AssetManager) :Movies {
         return Util.getJsonFromFile(assets,"movie_data.json")
